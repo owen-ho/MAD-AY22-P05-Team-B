cc4a3eb4ae0c06372d37c847a1e386a5e3b136eb
Ho Shang Luen Owen <s10205346@connect.np.edu.sg>
2022-05-30 21:08:36 +0800

    Initialization

---
+++ app/src/main/java/sg/edu/np/P05TeamB/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.P05TeamB;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
