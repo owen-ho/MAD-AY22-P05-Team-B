824f1aeac085dc73ddf10f1d720f690b2196f927
Jun Xian Owg <s10205779@connect.np.edu.sg>
2022-06-08 00:17:53 +0800

    Nav bar fragments

--- app/src/main/java/sg/edu/np/P05TeamB/MainActivity.java
+++ app/src/main/java/sg/edu/np/P05TeamB/MainActivity.java
@@ -1,14 +1,48 @@
 package sg.edu.np.P05TeamB;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import sg.edu.np.P05TeamB.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replacefragment(new homefrag());
+                    break;
+                case R.id.Shop:
+                    replacefragment(new shoppingfrag());
+                    break;
+                case R.id.Wishlist:
+                    replacefragment(new wishlistfrag());
+                    break;
+                case R.id.profile:
+                    replacefragment(new profilefrag());
+                    break;
+            }
+            return true;
+        });
+    }
+    private  void replacefragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
