c38f18943915fcb5b6ad8061615e69046a925647
Phua678 <94032313+Phua678@users.noreply.github.com>
2022-06-19 14:42:02 +0800

    Change the design of the change profile xml. Created a new activity user input price

---
+++ app/src/main/java/sg/edu/np/P05TeamB/UserInputPrice.java
@@ -0,0 +1,14 @@
+package sg.edu.np.P05TeamB;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInputPrice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_input_price);
+    }
+}
