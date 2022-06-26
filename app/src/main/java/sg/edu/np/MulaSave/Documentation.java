902b7f190b09237a01293592edc1f61071f6227b
qiren <s10205561@connect.np.edu.sg>
2022-06-27 01:35:07 +0800

    documentation activity

--- app/src/main/java/sg/edu/np/MulaSave/Documentation.java
+++ app/src/main/java/sg/edu/np/MulaSave/Documentation.java
@@ -3,6 +3,9 @@ package sg.edu.np.MulaSave;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Documentation extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Documentation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_documentation);
+        ((FloatingActionButton)findViewById(R.id.floatingBack)).setOnClickListener(new View.OnClickListener() {//finish activity on click of the back button
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
