ac739188f1f838c119ab30cb35f992785e5f000c
Jun Xian Owg <s10205779@connect.np.edu.sg>
2022-07-06 23:23:13 +0800

    description page

--- app/src/main/java/sg/edu/np/MulaSave/descriptionpage.java
+++ app/src/main/java/sg/edu/np/MulaSave/descriptionpage.java
@@ -3,6 +3,7 @@ package sg.edu.np.MulaSave;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class descriptionpage extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class descriptionpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_descriptionpage);
+
+
     }
 }
