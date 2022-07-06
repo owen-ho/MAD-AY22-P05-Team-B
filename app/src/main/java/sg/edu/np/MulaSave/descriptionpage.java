b4b1a0119830c636f9c72c5996e7aec8922f5d17
qiren <s10205561@connect.np.edu.sg>
2022-07-06 23:18:19 +0800

    start prodcut desc activity

--- app/src/main/java/sg/edu/np/MulaSave/descriptionpage.java
+++ app/src/main/java/sg/edu/np/MulaSave/descriptionpage.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class descriptionpage extends AppCompatActivity {
 
+    Product product;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_descriptionpage);
+
+        product = (Product) getIntent().getSerializableExtra("product");//get product from adapter
     }
 }
